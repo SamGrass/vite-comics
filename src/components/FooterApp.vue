3c4d2420669f4556f3d715ef9c31679edf62fed8
Samgrass <samuelegrassetti01@gmail.com>
2024-07-07 18:20:58 +0200

    first commit

---
+++ src/components/FooterApp.vue
@@ -0,0 +1,13 @@
+<script>
+    export default {
+        name: "FooterApp",
+    }
+</script>
+
+<template>
+    <div>
+        beppe
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/MainApp.vue
@@ -0,0 +1,13 @@
+<script>
+    export default {
+        name: "MainApp",
+    }
+</script>
+
+<template>
+    <div>
+        lorenzo
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
