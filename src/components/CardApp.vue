56545c2d961825a674175986e02b162d7ec84150
Samgrass <samuelegrassetti01@gmail.com>
2024-07-09 18:40:09 +0200

    creato componente card

---
+++ src/components/CardApp.vue
@@ -0,0 +1,43 @@
+<script>
+    export default {
+        name: 'CardApp',
+        props: {
+            thumb: String,
+            series: String
+
+        }
+            
+    }
+</script>
+
+
+<template>
+    <div class="card">
+        <div class="image-container">
+            <img :src="thumb" :alt="series">
+        </div>
+        <h5>{{series}}</h5>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+    .card {
+        width: calc(100% / 6 - 2rem);
+        
+
+        img {
+            display: block;
+            width: 100%;
+            aspect-ratio: 1/1;
+            object-fit: cover;
+        }
+
+        h5 {
+            text-align: start;
+            margin-top: 1rem;
+        }
+        
+    }
+    
+</style>
